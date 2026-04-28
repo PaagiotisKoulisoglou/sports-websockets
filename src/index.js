c019e86de4347cb76e993320e3691129b19975bf
PanagiotisKoulisoglou <panagiotiskoulisoglou@gmail.com>
2026-04-28 20:14:31 +0300

    Implement database schemas

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import express from 'express';
 
-const PORT = 8000;
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-// Use JSON middleware
+// Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('Server is up and running');
+  res.send('Sportes API is up!');
 });
 
 // Start server and log URL
 app.listen(PORT, () => {
   const url = `http://localhost:${PORT}`;
-  console.log(`Server started at ${url}`);
+  console.log(`Server is running at ${url}`);
 });
